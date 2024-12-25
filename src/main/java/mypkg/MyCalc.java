b6c627f646275c51bbe99dc01da6036375de099e
kalleshgit <kalleshrudrappamail@gmail.com>
2024-12-25 23:25:57 +0530

    mul3 added

--- src/main/java/mypkg/MyCalc.java
+++ src/main/java/mypkg/MyCalc.java
@@ -18,6 +18,10 @@ public class MyCalc {
 	{
 		return a*b;
 	}
+	int mul3 (int a, int b)
+	{
+		return a*b;
+	}
 
 	public static void main(String[] args) {
 		MyCalc calc= new MyCalc();
@@ -25,6 +29,7 @@ public class MyCalc {
 		System.out.println("Diff is : "+calc.diff(20, 10));
 		System.out.println("Diff is : "+calc.mul(20, 10));
 		System.out.println("Diff is : "+calc.mul2(10, 10));
+		System.out.println("Diff is : "+calc.mul3(20, 20));
 
 	}
 
